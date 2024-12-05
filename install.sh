340c9131f87e4c7833e6ea44be40839421f363e7
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2024-12-05 21:44:01 +0000

    conditionally install chezmoi, nvim

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -eu
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+if [ ! -e ~/.local/bin/chezmoi ]; then
+	sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+fi
 #bash -c "chezmoi init --apply https://github.com/karthikeyann/dotfiles.git"
 bash -c "chezmoi init --apply karthikeyann --force"
 echo "Installed dotfiles"
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz --output-dir /tmp
-tar -C ~/.local/ -xzf /tmp/nvim-linux64.tar.gz --strip-components=1
-echo "Installed nvim"
+if [ -e ~/.local/bin/nvim ]; then
+	echo "Already nvim installed"
+else
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz --output-dir /tmp
+	tar -C ~/.local/ -xzf /tmp/nvim-linux64.tar.gz --strip-components=1
+	echo "Installed nvim"
+fi
 
 # Above the fold
 ######################################################################################################################
